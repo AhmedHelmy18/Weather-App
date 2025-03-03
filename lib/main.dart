12c70e8ba4b5e549abca38791c35b2e3fd596179
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2025-03-03 20:10:20 +0200

    add bloc and cubit to the project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubit/weather_cubit/weather_cubit.dart';
 import 'package:weather_app/pages/home_page.dart';
-import 'package:weather_app/providers/weather_provider.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
+    BlocProvider(
       create: (context) {
-        return WeatherProvider();
+        return WeatherCubit(WeatherService());
       },
       child: WeatherApp(),
     ),
@@ -22,11 +23,11 @@ class WeatherApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         primarySwatch:
-            Provider.of<WeatherProvider>(context).weatherData == null
+            BlocProvider.of<WeatherCubit>(context).weatherModel == null
                 ? Colors.blue
-                : Provider.of<WeatherProvider>(
+                : BlocProvider.of<WeatherCubit>(
                   context,
-                ).weatherData!.getThemeColor(),
+                ).weatherModel!.getThemeColor(),
       ),
       home: HomePage(),
     );
